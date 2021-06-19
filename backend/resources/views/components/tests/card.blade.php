96b2325484efb9ca00dcaccadd9adfbc89cac371
goldenslumber <goldenslumbers0902@gmail.com>
2021-06-19 23:46:56 +0900

    属性バッグ

--- backend/resources/views/components/tests/card.blade.php
+++ backend/resources/views/components/tests/card.blade.php
@@ -1,11 +1,13 @@
-@props(
+@props([
 'title',
-'message' => '初期値です'
-'contwnt' => '本文初期値です';
+'message' => '初期値です',
+'content' => '本文初期値です'
 
 ])
 
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+    ]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
