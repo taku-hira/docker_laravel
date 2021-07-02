cd0ffa15b796a16dead72a61c8b3f93c0958ef74
goldenslumber <goldenslumbers0902@gmail.com>
2021-07-02 23:20:34 +0900

    edit

--- backend/app/Http/Controllers/Owner/ShopController.php
+++ backend/app/Http/Controllers/Owner/ShopController.php
@@ -52,12 +52,19 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
+
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|information|max:1000',
+            'is_selling' => 'required',
+        ]);
+
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid()){
-            Storage::putFile('public/shops', $imageFile); // リサイズなしの場合
+            $fileNameToStore = Storage::putFile('public/shops', $imageFile); // リサイズなしの場合
 
             // $filename = ImageService::upload($imageFile, 'shops');
-            
+
             //リサイズありの場合      (エラーが出るためリサイズなしで処理します)
             // $fileName = uniqid(rand().'_');
             // $extension = $imageFile->extension();
@@ -69,7 +76,22 @@ class ShopController extends Controller
             // Storage::put('public/shops' . $fileNameToStore, $resizedImage);
         }
 
-        return redirect()->route('owner.shops.index');
+        $shop  = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $shop->filename = $fileNameToStore;
+
+        }
+
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with(['message' => '店舗情報を更新しました', 
+        'status' => 'info']);
     }
 
 }
